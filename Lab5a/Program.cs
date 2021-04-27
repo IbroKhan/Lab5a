4d04c7410dd5e5ff550aa4f993e980bb3e908beb
Ibrahim Khan <M.KHAN-2020@hull.ac.uk>
2021-04-27 03:10:25 +0100

    Loads in a Sums xml file and processes all the matty goodmess

--- Lab5a/Program.cs
+++ Lab5a/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml;
 
 namespace Lab5a
 {
@@ -6,8 +7,122 @@ namespace Lab5a
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if(args.Length > 0)
+            {
+                string filename = args[0];
+                XmlDocument inputFile = new XmlDocument();
+                inputFile.Load(filename);
+                LoadDocument(inputFile);
+            }
+            else
+            {
+                Console.WriteLine("No progrem arguments were provided. You should at least include the input file name.");
+                Console.WriteLine("Program exiting.");
+                return;
+            }
+        }
+        static void LoadDocument(XmlDocument pinputFile)
+        {
+            XmlNode declarationNode = pinputFile.FirstChild;
+            XmlNode sumsNode = declarationNode.NextSibling;
+            LoadSums(sumsNode);
+        }
+        static void LoadSums(XmlNode pSumsNode)
+        {
+            XmlNodeList sumNodes = pSumsNode.ChildNodes;
+            int numberofSums = sumNodes.Count;
+            Console.WriteLine("Loading " + numberofSums + " sums.");
+            for(int i = 0; i < numberofSums; i++)
+            {
+                Console.WriteLine("Loading sum: " + i);
+                XmlNode sumNode = sumNodes[i];
+                double result = LoadChildNode(sumNode);
+                Console.WriteLine(" = " + result);
+            }
+        }
+       
+        static double LoadChildNode(XmlNode pNode)
+        {
+            if(pNode.Name == "number")
+            {
+                return LoadNumberNode(pNode);
+            }
+            else
+            {
+                return LoadOperatorNode(pNode);
+            }
+        }
+        static double LoadNumberNode(XmlNode pNumberNode)
+        {
+            double number = double.Parse(pNumberNode.FirstChild.Value);
+            Console.WriteLine(number);
+            return number;
+        }
+        static double LoadOperatorNode(XmlNode pOperatorNode)
+        {
+            double result = 0;
+            XmlNodeList childNodes = pOperatorNode.ChildNodes;
+            for(int i = 0; i < childNodes.Count; i++)
+            {
+                XmlNode childNode = childNodes[i];
+                double childResult = LoadChildNode(childNode);
+                if(i == 0)
+                {
+                    result = childResult;
+                    OutputOperatorString(pOperatorNode.Name);
+                }
+                else
+                {
+                    result = PerformOperator(pOperatorNode.Name, result, childResult);
+                    if(i != childNodes.Count - 1)
+                    {
+                        OutputOperatorString(pOperatorNode.Name);
+                    }
+                    
+                }
+                
+            }
+            return result;
+        }
+        static double PerformOperator(string pNodeType, double pResult, double pChildResult)
+        {
+            if(pNodeType == "add")
+            {
+                pResult += pChildResult;
+            }
+            else if (pNodeType == "subtract")
+            {
+                pResult -= pChildResult;
+            }
+            else if (pNodeType == "multiply")
+            {
+                pResult *= pChildResult;
+            }
+            else if (pNodeType == "divide")
+            {
+                pResult /= pChildResult;
+            }
+            
+            return pResult;
+        }
 
+        static void OutputOperatorString(string pNodeType)
+        {
+            switch (pNodeType)
+            {
+                case "add":
+                    Console.Write(" + ");
+                    break;
+                case "subtract":
+                    Console.Write(" - ");
+                    break;
+                case "multiply":
+                    Console.Write(" * ");
+                    break;
+                case "divide":
+                    Console.Write(" / ");
+                    break;
+            }
         }
     }
 }
